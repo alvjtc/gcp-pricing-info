6998d2b9f9794fa886f130d1a0c7325dd5fbf1ca
Alvaro Teijido <alvjtc@gmail.com>
2021-07-17 11:40:02 +0200

    feat(compute): add price for n1 & n2 in Belgium

--- internal/api/compute/data.go
+++ internal/api/compute/data.go
@@ -14,14 +14,7 @@
 
 package compute
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-)
-
-type skuDataJSON struct {
+/*type skuDataJSON struct {
 	Compute struct {
 		Region []struct {
 			Name   string `json:"name"`
@@ -67,4 +60,32 @@ func InitData() (skuDataJSON, error) {
 	}
 
 	return data, nil
+}*/
+
+type SKU = map[string]map[string]map[string]string
+
+var SKUData SKU
+
+func InitData() SKU {
+	SKUData = make(map[string]map[string]map[string]string)
+
+	SKUData["europe-west1"] = make(map[string]map[string]string)
+
+	SKUData["europe-west1"]["n1"] = make(map[string]string)
+	SKUData["europe-west1"]["n1"]["cpu"] = "9431-52B1-2C4F"
+	SKUData["europe-west1"]["n1"]["cpu1y"] = "4F49-1FC5-D994"
+	SKUData["europe-west1"]["n1"]["cpu3y"] = "20F3-B410-FB36"
+	SKUData["europe-west1"]["n1"]["ram"] = "39F4-0112-6F39"
+	SKUData["europe-west1"]["n1"]["ram1y"] = "0FCC-C885-6989"
+	SKUData["europe-west1"]["n1"]["ram3y"] = "B1FD-24D4-0892"
+
+	SKUData["europe-west1"]["n2"] = make(map[string]string)
+	SKUData["europe-west1"]["n2"]["cpu"] = "9F61-45D7-D4FB"
+	SKUData["europe-west1"]["n2"]["cpu1y"] = "A121-1D02-4CFA"
+	SKUData["europe-west1"]["n2"]["cpu3y"] = "1438-08DD-CC18"
+	SKUData["europe-west1"]["n2"]["ram"] = "A109-54C1-7CB0"
+	SKUData["europe-west1"]["n2"]["ram1y"] = "E6C5-0BFA-F6A6"
+	SKUData["europe-west1"]["n2"]["ram3y"] = "FEA1-DB7A-4C41"
+
+	return SKUData
 }
