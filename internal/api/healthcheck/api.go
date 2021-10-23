f2c6f15df65e4663524ccb0555bc876558c86bae
Alvaro Teijido <ateijido@intelligencepartner.com>
2021-10-23 19:48:01 +0200

    feat(healthcheck): add more info

--- internal/api/healthcheck/api.go
+++ internal/api/healthcheck/api.go
@@ -21,7 +21,7 @@ func Handler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	_, err := w.Write([]byte(`{"message":"Server is up and running"}`))
+	_, err := w.Write([]byte(`{"message":"server is up and running","status":"OK"}`))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
